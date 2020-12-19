64f87c8fb55bb73fd6bcad4ed551a4424aa82336
Denys Fenchenko <denisfench@gmail.com>
2020-12-19 09:34:39 -0500

    Adding clean Backward Euler's method with all of the comments

--- Brusselator.m
+++ Brusselator.m
@@ -19,112 +19,93 @@ dxt = @(x, y) 2 * y - x;
 dyt = @(x, y) x;
 % forwardEulerTest = forwdEul(dxt, dyt, 1, 0, 1, 50);
 
-backEul = backEuler(xdt, ydt, 1, 1, 1, 3, 0.01, 30);
+backEul = backEuler(1, 1, 1, 3, 0.01, 30);
 % frwdEul = forwdEul(1, 1, 1, 3, 0.1, 30);
 
-% plotting the solution of Brusselator problem using the Forward Euler 
-% method 
-
-% function which implements the Backward Euler method
-function z = backEuler(fx, fy, X_init, Y_init, A, B, dt, ft)
-num_iter = ft/dt; % the number of iterations is the 
+% the backEuler function implements the Backward Euler method for Bruesselator
+% problem, taking the initial concentrations X_0 and Y_0, constants A, B, 
+% and the iteration time step df. ft is the final time T, T = 30.
+function z = backEuler(X_init, Y_init, A, B, dt, ft)
+num_iter = ft/dt; % the number of iterations for the backwardEler
+% z = [time step, X value, Y value]
 z = zeros(num_iter, 3);
-z(1, 1) = 0; % time why is the first time step 1, not 0? it should be 0 and
-% increase in increments of dt 
-z(1, 2) = X_init; % x initial 
-z(1, 3) = Y_init; % y initial 
-for i = 1:num_iter - dt
-     % using newton's method to find the value of (z^{n+1})
+z(1, 1) = 0; % initialize time = 0
+z(1, 2) = X_init; % X initial 
+z(1, 3) = Y_init; % Y initial 
+% an actual Backward Euler loop, up to the final time T = 30 
+for i = 1:num_iter - dt 
+     % using Newton's method to solve the nonlinear system, in order to 
+     % find the value of (Z^{n+1})
      newtn = newton(z(i, 2), z(i, 3), A, B, 10, dt);
-     disp(newtn);
-     % temp = z(i, [2, 3]) + t_delta * bruesselator(z(i, 2), z(i, 3), A, B);
-     % disp(z(i,:));
-     % update the value of x 
-     z(i+1, 2) = newtn(1, 1);
-     % update the value of y 
-     z(i+1, 3) = newtn(1, 2);
-     % set the value of time step T
-     z(i+1, 1) = z(i, 1) + dt;
+     z(i+1, 2) = newtn(1, 1); % adding the value of x at the next time step 
+     z(i+1, 3) = newtn(1, 2); % adding the value of y at the next time step
+     z(i+1, 1) = z(i, 1) + dt; % adding the next time step 
 end 
-% plotting the time T vs forward Euler approximation
-figure("Name", "Backward Euler: plot of X and Y vs time");
-% plotting time vs x 
+% plotting the time step t vs the backward Euler approximation at the 
+% corresponding step t 
+figure("Name", "Backward Euler: plot of time vs X and Y");
+% plotting time vs X
 plot(z(:, 1), z(:, 2), 'color', 'red', 'LineWidth', 2.0);
 xlabel("T");
 ylabel("X, Y");
 hold on;
-% plotting time vs y 
+% plotting time vs Y
 plot(z(:, 1), z(:, 3), 'color', 'blue', 'LineWidth', 2.0);
 legend("species X", "species Y");
 hold off;
 end
 
-% testing the newton's method 
-% doing 5 iterations 
+% the newton function solves the nonlinear equation for the Backward Euler
+% method, it takes X_init and Y_init as parameters, 
+% which are the apporximations to the values of X and Y at a given time step 
+% at which newton is called, A, B are constants for our ODE, t is the time 
+% increment
 function sol = newton(X_init, Y_init, A, B, num_iter, t)
-temp = zeros(2, 1);
-G = zeros(2, 1);
-F = zeros(2, 1);
+% w = [X_{n+1}^{k}, X_{n+1}^{k}]
 w = zeros(num_iter, 2);
 % entry 1 corresponds to x value, entry 2 corresponds to y value 
-w(1, 1) = X_init;
-w(1, 2) = Y_init;
+w(1, 1) = X_init; % initialize the X guess for the Newton's method, which 
+% corresponds to the value of X retuned by the backward Euler method at the 
+% previous iteration 
+w(1, 2) = Y_init; % initialize the Y guess 
+% loop num_iter times 
 for i = 1:num_iter-1
-    % precomputing G(Z_{n+1}^k), Z^n = [X_intit, Y_init]
-    % computing F(Z_{n+1}^k)
+    % evaluating the system of ODEs at X_{n+1}^{k}, Y_{n+1}^{k}
     F = bruesselator(w(i, 1), w(i, 2), A, B);
+    % evaluating the backward Euler nonlinear equation at X_{n+1}^{k}, Y_{n+1}^{k}
     G(1, 1) = w(i, 1) - X_init - t * F(1, 1);
     G(2, 1) = w(i, 2) - Y_init - t * F(2, 1);
-    % temp = transpose(w(i, :)) - inv(jacobi(w(i, 1), w(i, 2), B, t)) * bruesselator(w(i, 1), w(i, 2), A, B);
+    % performing the actual Newton's solve  
     temp = transpose(w(i, :)) - inv(jacobi(w(i, 1), w(i, 2), B, t)) * G;
-    % disp(temp)
-    w(i+1, 1) = temp(1, 1);
-    w(i+1, 2) = temp (2, 1);
+    w(i+1, 1) = temp(1, 1); % adding the value of X at the next iteration
+    w(i+1, 2) = temp (2, 1); % adding the value of Y at the next iteration
 end
 sol = w(num_iter, :);
 end 
 
-% defining function f_xdt as a function handle
+
+% defining the function dX/dt of the first ODE
 function f_xdt = f_xdt(x, y, a, b)
 f_xdt = a + x^2 * y - b * x - x;
 end 
 
-% defining function f_xdt as a function handle
+
+% defining function dY/dt of the second ODE
 function f_ydt = f_ydt(x, y, b)
 f_ydt = b * x - x^2 * y;
 end 
 
-% Bruesselator problem definition
-function df = bruesselator(x, y, a, b)
+
+% using dX/dt and dY/dt to construct the sytem of ODEs
+function F = bruesselator(x, y, a, b)
 F = zeros(2, 1);
 F(1) = f_xdt(x, y, a, b);
 F(2) = f_ydt(x, y, b);
-df = F;
-%disp("The Brusselator function is");
-%disp(df);
 end 
 
 
-function h = testFunc(x, y)
-h = zeros(2, 1);
-h(1) = 2 * y - x;
-h(2) = x;
-end 
-
-
-% jacobi function computes the Jacobian matrix given parameters 
-% X, Y, B, and num_iter
-% I believe the Jacobian below id incorrect 
-% function J = jacobi(X, Y, B)
-% J = zeros(2, 2);
-%     J(1, 1) = 2*X*Y - B - 1;
-%     J(1, 2) = X^2;
-%     J(2, 1) = B - 2*X*Y;
-%     J(2, 2) = - X^2;
-% end
-
-% The function below implements the Jacobian matrix for the function 
-% G(Z^{n+1})
+% The function below implements the Jacobian matrix for the Newton's step
+% the Jacobian is for the fucntion G(Z^{n+1})
 function J = jacobi(X, Y, B, t)
     J = zeros(2, 2);
     J(1, 1) = 1 - 2 * X * Y * t + B * t + t;
